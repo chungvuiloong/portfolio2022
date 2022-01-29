e7160ea1abdfe035468cfd280de89aac1a050026
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-01-29 19:28:56 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function meowUs() {
+  // 1st Prototype - Works!!! BUt need to get ID from HTML
+  // var meow = document.getElementById("meow");
+
+  return (
+    meow.play() + alert("Thank you for meowing us! We will meow back ASAP!")
+  );
+}
