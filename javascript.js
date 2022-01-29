1db1fc385acc7b73fdff9cd7a926624f122d66d7
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-01-29 23:13:04 +0200

    100

--- javascript.js
+++ javascript.js
@@ -86,13 +86,13 @@ ageCheck = () => {
   } else if (ageValue == "") {
     alert("Please enter your age.");
     ageHighlight.setAttribute("class", "invalid");
+  } else if (ageValue >= 100) {
+    alert("Are you really that old?");
+    ageHighlight.setAttribute("class", "valid");
   } else if (ageValue > 38) {
     alert(
       "The oldest living cat, Creme Puff, is 38 years old. You have broken the record for oldest cat! If you are a cat...."
     );
-    ageHighlight.setAttribute("class", "valid");
-  } else if (ageValue > 100) {
-    alert("Are you really that old?");
     ageHighlight.setAttribute("class", "valid");
   } else {
     ageHighlight.setAttribute("class", "valid");
