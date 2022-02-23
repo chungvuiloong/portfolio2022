4b5089dd83d8e404d0f42b39c38c185761372d5c
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-02-23 13:31:27 +0200

    transferred all the parallex materials

--- javascript.js
+++ javascript.js
@@ -3,6 +3,30 @@ var numberFilter = /^[0-9]+$/;
 var emailFilter =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+// Animation for hero banner
+let darkSky = document.querySelector("#darkSky");
+let stars = document.querySelector("#stars");
+let moon = document.querySelector("#whiteMoon");
+let meteor = document.querySelector("#meteor");
+let backMountains = document.querySelector("#backMountains");
+let bottomCloud = document.querySelector("#bottomCloud");
+
+window.addEventListener("scroll", () => {
+  let yPosition = window.scrollY;
+
+  stars.style.left = yPosition * 0.05 + "px";
+  stars.style.top = yPosition * 0.25 + "px";
+
+  meteor.style.left = yPosition * 1 + "px";
+  meteor.style.top = yPosition * 1 + "px";
+
+  //   blackMoon.style.marginTop = yPosition * 0.6 + "px";
+  moon.style.marginTop = yPosition * 1.25 + "px";
+  bottomCloud.style.top = yPosition * 0.25 + "px";
+  backMountains.style.top = yPosition * 0.1 + "px";
+});
+
+// Press Submit and it meows
 meowUs = () => {
   // 1st Prototype - Works!!! BUt need to get ID from HTML
   // var meow = document.getElementById("meow");
